68ae8808dbaf12a4aece7f2d891eede9a072e95b
GigaChadVGM <gigachadvgm@gmail.com>
2024-12-03 22:58:15 +0100

    added external path generator

---
+++ external_path_generator/ExternalPathGenerator.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
+from folder_analyser import FolderAnalyser
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+def ExternalPathGenerator(paths: list):
+    """
+    Generates a string including the selected file_path as well as its size for multiple paths.
+    :param paths: list of file or directory paths
+    """
+    target_folder = "/dropzone/"
+    return_text = ""
+
+    for full_file_path in paths:
+        normalized_path = full_file_path.replace('\\', '/').lower()
+        if target_folder.lower() in normalized_path:
+            index = normalized_path.find(target_folder) + len(target_folder)
+            relative_path = full_file_path[index:].replace('\\', '/')
+        else:
+            raise ValueError(
+                f"The directory '{target_folder.strip('/')}' cannot be found in '{full_file_path}'")
+
+        file_size = os.path.getsize(full_file_path) - 128
+
+        file_element = ET.Element("file")
+        file_element.set("name", relative_path)
+        file_element.set("size", str(file_size))
+
+        xml_string = ET.tostring(file_element, encoding="unicode")
+        print(xml_string)
+
+        return_text += xml_string + '\n'
+    return return_text
+
+
+if __name__ == "__main__":
+    paths = sys.argv[1:]
+    paths_auto = FolderAnalyser(paths)
+    if paths_auto.exists:
+        try:
+            ExternalPathGenerator(paths_auto.file_list)
+            input(GREEN + "\nProcess Successful, press enter to exit." + RESET)
+        except Exception as e:
+            print(RED + f"\nError : {e}" + RESET)
+            input(RED + "Process Failed, press enter to exit." + RESET)
---
+++ external_path_generator/ExternalPathGeneratorOutputTXT.py
@@ -0,0 +1,28 @@
+import sys
+from ExternalPathGenerator import ExternalPathGenerator as EPG
+from folder_analyser import FolderAnalyser
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+
+def save_as_file(path):
+    try:
+        text = EPG(path)
+        with open('output external paths.txt', 'w', encoding='utf-8') as fichier:
+            fichier.write(text)
+        print(f"The file was saved successfully in the file's directory.")
+    except Exception as e:
+        print(f"Une erreur est survenue lors de la sauvegarde : {e}")
+
+
+if __name__ == "__main__":
+    paths = sys.argv[1:]
+    if FolderAnalyser(paths).exists:
+        try:
+            save_as_file(paths)
+            input(GREEN + "\nProcess Successful, press enter to exit." + RESET)
+        except Exception as e:
+            print(RED + f"\nError : {e}" + RESET)
+            input(RED + "Process Failed, press enter to exit." + RESET)
