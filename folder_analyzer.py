712e5dd79122c465bec8a59038c52ad0e7ba9d54
GigaChadVGM <gigachadvgm@gmail.com>
2024-12-09 11:19:25 +0100

    Update folder_analyzer.py

--- folder_analyzer.py
+++ folder_analyzer.py
@@ -2,18 +2,27 @@ import os
 from colors import *
 
 class FolderAnalyzer:
-    def __init__(self, paths, is_xml_generator: bool = False):
+    def __init__(self, paths, paths_type: str, is_xml_generator: bool = False):
         """
         Class that gives all the files without folders in a selection of folders
         :param paths: path of one or many folders
         """
         if not paths:
-            print(RED + "No files or directories were provided." + RESET)
-            input(RED + "\nProcess Failed, press enter to exit." + RESET)
-            self.exists = False
-            return
+            raise Exception(RED + "No files or directories were provided." + RESET)
         else:
-            self.exists = True
+            if paths_type == "folder":
+                if not os.path.isdir(paths):
+                    self.exists = False
+                    raise NotADirectoryError(RED + "The provided path is not a directory." + RESET)
+
+            elif paths_type == "file":
+                if not os.path.isfile(paths):
+                    self.exists = False
+                    raise FileNotFoundError(RED + "The provided path is not a file." + RESET)
+
+            elif paths_type == "multiple":
+                # means that all the provided paths are files and folders, the check will always be true
+                pass
 
         paths = paths if isinstance(paths, list) else [paths]
 
@@ -34,7 +43,7 @@ class FolderAnalyzer:
                         else:
                             list_paths.append(os.path.join(root_dir, file))
             else:
-                list_paths = paths
+                list_paths.append(element)
 
             self.file_list = list_paths
 
